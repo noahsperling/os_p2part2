7c2af63bb3a59b81493f80cbf8e69b9ca4bba062
sperling19 <sperling19@up.edu>
2018-04-03 11:07:36 -0700

    commented address.cpp

--- address.cpp
+++ address.cpp
@@ -15,8 +15,14 @@ int main(int argc, char *argv[]) {
 
   // casts address from string to int
   int address = atoi(argv[1]);
+
+  // makes a copy
   int a_copy = address;
+
+  // to store the binary version of address
   char bin_addr[] = "00000000000000000000000000000000";
+
+  //converts address to binary
   int i;
   for(i = 31; i > -1; i--) {
     if (a_copy % 2) {
@@ -24,9 +30,12 @@ int main(int argc, char *argv[]) {
     }
     a_copy /= 2;
   }
+
+  // to store offset and page number
   char offset[] = "000000000000";
   char page[] = "00000000000000000000";
 
+  // copies binary offset and page number
   for(i = 31; i > -1; i--) {
     if(i >= 20) {
       offset[i - 20] = bin_addr[i];
@@ -35,9 +44,11 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  // converts offset and page number to ints
   int p_int = std::stoi(page, nullptr, 2);
   int o_int = std::stoi(offset, nullptr, 2);
 
+  // prints results
   cout << "The address " << address << " contains: " << endl;
   cout << "Page Number: " << p_int << endl;
   cout << "Offset: " << o_int << endl;

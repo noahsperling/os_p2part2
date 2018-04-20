7cd79f66231730da9e35eb6dc94c4f0fa87d5c6a
nowlin19 <nowlin19@up.edu>
2018-04-20 16:48:32 -0700

    Customer function and threading implemented

--- bankers.cpp
+++ bankers.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-#include <stdlib.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <unistd.h>
 
 /*
 * @authors Noah Sperling and Alexander Nowlin
@@ -12,16 +16,18 @@ using namespace std;
 #define NUMBER_OF_CUSTOMERS 2
 #define NUMBER_OF_RESOURCES 2
 
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 int available[NUMBER_OF_RESOURCES];
 
 int maximum[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES] = {{2,2},{2,2}};
 
-int allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES] = {{0,0},{0,0}};
+int allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES] = {{1,0},{1,1}};
 
-int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES] = {{2,2},{2,2}};
+int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES] = {{1,2},{1,1}};
 
-bool safety()
-{
+bool safety() {
   int work[NUMBER_OF_RESOURCES];
   bool finish[NUMBER_OF_CUSTOMERS];
 
@@ -33,6 +39,7 @@ bool safety()
     finish[i] = false;
   }
 
+  /*
   cout << "finish array : ";
   for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
     if(i != NUMBER_OF_CUSTOMERS - 1) {
@@ -41,6 +48,7 @@ bool safety()
       cout << finish[i] << endl;
     }
   }
+  */
 
   // whether an index has been found s.t. need <= work
   bool temp1 = true;
@@ -52,35 +60,29 @@ bool safety()
     temp1 = false;
 
     // loops through indices to find one where need <= work
-    for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++)
-    {
+    for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
       bool temp = false;
 
       // if this one has already been assessed, continue
-      if (finish[i])
-      {
+      if (finish[i]) {
         continue;
       }
 
       // checks an index to see if need <= work
-      for (int j = 0; j < NUMBER_OF_RESOURCES; j++)
-      {
-        if (need[i][j] <= work[j])
-        {
+      for (int j = 0; j < NUMBER_OF_RESOURCES; j++) {
+        if (need[i][j] <= work[j]) {
           temp = true;
         }
-        else
-        {
+        else {
           temp = false;
-          cout << "need is not less than work at this index!" << endl;
+          // cout << "need is not less than work at this index!" << endl;
           break;
         }
       }
 
       // at this point, it has been determined that need <= work
-      if (temp)
-      {
-        cout << "updating work and finish!" << endl;
+      if (temp) {
+        // cout << "updating work and finish!" << endl;
         // updates work
         for (int k = 0; k < NUMBER_OF_RESOURCES; k++) {
           work[k] = work[k] + allocation[i][k];
@@ -89,7 +91,7 @@ bool safety()
         finish[i] = true;
 
         // we found an index
-        cout << "We found an index where need <= work, so iterate again!" << endl;
+        // cout << "We found an index where need <= work, so iterate again!" << endl;
         temp1 = true;
         break;
       }
@@ -127,9 +129,19 @@ int resource_request(int customer, int request[]){
     }
   }
 
-  int[] old_available = available;
-  int[][] old_allocation = allocation;
-  int[][] old_need = need;
+  int old_available[NUMBER_OF_RESOURCES];
+  int old_allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+  int old_need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+
+  for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
+    for (int j = 0; j < NUMBER_OF_RESOURCES; j++) {
+      if (i == 0) {
+        old_available[j] = available[j];
+      }
+      old_allocation[i][j] = allocation[i][j];
+      old_need[i][j] = need[i][j];
+    }
+  }
 
 
   for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
@@ -140,29 +152,121 @@ int resource_request(int customer, int request[]){
 
   bool is_safe = safety();
   if (!is_safe) {
-    available = old_available;
-    need = old_need;
-    allocation = old_allocation;
+    cout << "System is not safe, resetting values" << endl;
+    for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
+      for (int j = 0; j < NUMBER_OF_RESOURCES; j++) {
+        if (i == 0) {
+          available[j] = old_available[j];
+        }
+        allocation[i][j] = old_allocation[i][j];
+        need[i][j] = old_need[i][j];
+      }
+    }
     return 0;
   }
 
   return 1;
 }
 
-int main(int argc, char* argv[])
-{
-  if (argc != NUMBER_OF_RESOURCES + 1)
-  {
+int resource_release(int customer, int release[]) {
+  for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
+    if (allocation[customer][i] < release[i]) {
+      cout << "Invalid release request" << endl;
+      return -1;
+    }
+  }
+  for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
+    allocation[customer][i] -=  release[i];
+  }
+  cout << "Resources released" << endl;
+  return 1;
+}
+
+
+void *customer(void *arg) {
+  int *temp = (int *)arg;
+  int index = *temp;
+
+  cout << "Starting while loop for customer " << index << endl;
+  while (true) {
+    int r_or_r = rand() % 10;
+    if (r_or_r > 5) {
+      //request
+      cout << "customer " << index << " making a request" << endl;
+      int r[NUMBER_OF_RESOURCES];
+      for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
+        if (need[index][i] > 1) {
+          r[i] = rand() % need[index][i];
+        } else if (need[index][i] == 1) {
+          r[i] = 1;
+        }
+      }
+      int temp = pthread_mutex_trylock(&mutex);
+      if (temp != 0) {
+        continue;
+      }
+      resource_request(index, r);
+      pthread_mutex_unlock(&mutex);
+    } else {
+      //release
+      cout << "customer " << index << " making a release" << endl;
+      int r[NUMBER_OF_RESOURCES];
+      for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
+        if (allocation[index][i] > 1) {
+          r[i] = rand() % allocation[index][i];
+        } else if (allocation[index][i] == 1) {
+          r[i] = 1;
+        }
+      }
+      int temp = pthread_mutex_trylock(&mutex);
+      if (temp != 0) {
+        cout << "Mutex locked, waiting" << endl;
+        continue;
+      }
+      resource_request(index, r);
+      pthread_mutex_unlock(&mutex);
+    }
+    sleep(1.5);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != NUMBER_OF_RESOURCES + 1) {
     cout << "Invalid number of arguments" << endl;
     return 0;
   }
 
-  for (int i = 0; i < NUMBER_OF_RESOURCES; i++)
-  {
+  cout << "Hello" << endl;
+
+  // seed random function
+  srand(time(NULL));
+
+  for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
     available[i] = atoi(argv[i+1]);
   }
 
-  cout << "System is Safe: " << safety() << endl;
+  pthread_t customers[NUMBER_OF_CUSTOMERS];
+
+  cout << "Initializing threads" << endl;
+  for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
+    int * temp = (int *)malloc(sizeof(int));
+    *temp = i;
+    cout << "Initializing thread " << i << endl;
+    int return_val = pthread_create(&customers[i], NULL, customer, (void *)(temp));
+  }
+
+  sleep(10);
+
+  for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
+    pthread_cancel(customers[i]);
+  }
+  /*
+  int request[NUMBER_OF_RESOURCES] = {1,0};
+
+  int release = resource_release(0, request);
+
+  cout << "System is Safe: " << resource_request(0, request) << endl;
   //pthread_t customers[NUMBER_OF_CUSTOMERS];
-  return 0;
+  */
+
 }

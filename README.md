9e3381223b7e278b4140af7581a6cbd0689eaa12
sperling19 <sperling19@up.edu>
2018-04-21 14:31:11 -0700

    Fixed posix and added to README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # os_p2part2
 
-When compiling bankers.cpp using g++,  use the argument -std=c++11 
+When compiling bankers.cpp and posix.cpp using g++, use the argument -std=c++11 
--- posix.cpp
+++ posix.cpp
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <random>
+#include <chrono>
+#include <thread>
+#include <unistd.h>
 
 /*
 * @authors Noah Sperling and Alexander Nowlin
@@ -12,81 +15,122 @@
 
 using namespace std;
 
-// semaphore to lock bridge traveling
-static bool semaphore;
+// mutex to lock bridge traveling
+pthread_mutex_t bridge_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 static int prev_dir = 0; // 0 for north, 1 for south
 
 // number of farmers for north and south
 static int num_north;
 static int num_south;
 
+static int running_threads;
+
+
 // function to sleep for a random time, simulating crossing the bridge
 void sleep_rand_time() {
-  int sec = rand() % 2;
+  int sec = rand() % 5;
+  if (sec == 0) {
+    sec = 1;
+  }
   cout << "sleeping " << sec << " seconds"<< endl;
   sleep(sec);
   return;
 }
 
+
 // function for a north farmer crossing the bridge
-void north() {
-  while (semaphore) {
+void *north(void *arg) {
+  while (true) {
     // if brige not locked and previous farmer that crossed was south farmer
     // or there are no south farmers waiting to cross
-    if (!semaphore && (prev_dir == 1 || num_south == 0)) {
-      // lock the bridge
-      semaphore = true;
-      break;
+
+    int temp = pthread_mutex_trylock(&bridge_mutex);
+    if (temp != 0) {
+      continue;
+    } else {
+      if (prev_dir == 1 || num_south == 0) {
+        break;
+      } else {
+        pthread_mutex_unlock(&bridge_mutex);
+        continue;
+      }
     }
   }
   // north farmer crossing bridge
   cout << "Going North!" << endl;
   sleep_rand_time();
 
-  // decrease number of north farmers
+  running_threads--;
+
+  // decrease number of northbound farmers
   num_north--;
 
+  prev_dir = 0;
+
   // unlock bridge
-  semaphore = false;
-  return;
+  pthread_mutex_unlock(&bridge_mutex);
+  return 0;
 }
 
+
 // function for a south farmer crossing the bridge
-void south() {
-  while (semaphore) {
+void *south(void* arg) {
+  while (true) {
     // if bridge not locked and previous farmer that crossed was north farmer
     // or there are no norhter farmers waiting to cross
-    if (!semaphore && (prev_dir == 0 || num_north == 0)) {
-      // lock the bridge
-      semaphore = true;
-      break;
+    int temp = pthread_mutex_trylock(&bridge_mutex);
+    if (temp != 0) {
+      continue;
+    } else {
+      if (prev_dir == 0 || num_north == 0) {
+        break;
+      } else {
+        pthread_mutex_unlock(&bridge_mutex);
+        continue;
+      }
     }
   }
   // south farmer crossing bridge
   cout << "Going South!" << endl;
   sleep_rand_time();
 
-  // decrease number of south farmers
+  running_threads--;
+
+  // decrease number of southbound farmers
   num_south--;
 
+  prev_dir = 1;
+
   // unlock the bridge
-  semphore = false;
-  return;
+  pthread_mutex_unlock(&bridge_mutex);
+  return 0;
 }
 
+
 int main(int argc, char *argv[]) {
+  cout << "Main called" << endl;
 
   // check for valid number of arguments
   if (argc != 3) {
     cout << "Invalid Arguments.  Re-run with the number of northbound farmers and then the number of southbound farmers." << endl;
+    return -1;
   }
 
   // store the arguments as the number of north and south farmers
   num_north = atoi(argv[1]);
   num_south = atoi(argv[2]);
 
-  // initialize bridge to unlocked
-  semaphore = false;
+  if (num_north < 0 || num_south < 0) {
+    cout << "Negative numbers of farmers are invalid!" << endl;
+    return 0;
+  } else if (num_north == 0 && num_south == 0) {
+    cout << "No farmers created!" << endl;
+    return 0;
+  }
+
+  cout << "Northbound Farmers: " << num_north << endl;
+  cout << "Southbound Farmers: " << num_south << endl;
 
   // seeds the rand() function
   srand(time(NULL));
@@ -96,49 +140,29 @@ int main(int argc, char *argv[]) {
   pthread_t south_threads[num_south];
 
   // index for north and south farmer arrays
-  cur_north_index = num_north - 1;
-  cur_south_index = num_south - 1;
-  return_val;
+  int cur_north_index = num_north - 1;
+  int cur_south_index = num_south - 1;
+  int return_val;
 
-  while(num_north != 0 || num_south != 0) {
+  pthread_mutex_lock(&bridge_mutex);
+  while(cur_north_index >= 0 || cur_south_index >= 0) {
+    // if there are south farmers waiting to cross
+    if (cur_south_index >= 0) {
+      // send south farmer across bridge
+      return_val = pthread_create(&south_threads[cur_south_index], NULL, south, (void *)NULL);
+      running_threads++;
 
-    // if north farmer just went
-    if(prev_dir == 0) {
-      // if there are south farmers waiting to cross
-      if (num_south > 0) {
-        // send south farmer across bridge
-        return_val = pthread_create(&south_threads[cur_south_index], NULL, south, NULL);
+      // decrement the index to represent that farmers have crossed the bridge
+      cur_south_index--;
+    } else if (cur_north_index >= 0) {
+      // no south farmers waiting, but north farmers waiting
 
-        // decrement the index to represent that farmers have crossed the bridge
-        cur_south_index--;
-      } else if (num_north > 0) {
-        // no south farmers waiting, but north farmers waiting
+      // send north farmer across bridge
+      return_val = pthread_create(&north_threads[cur_north_index], NULL, north, (void *)NULL);
+      running_threads++;
 
-        // send north farmer across bridge
-        return_val = pthread_create(&north_threads[cur_north_index], NULL, north, NULL);
-
-        // decrement the index to represent that farmers have crossed the bridge
-        cur_north_index--;
-      }
-    } else if(prev_dir == 1) {
-      // if south farmer just went
-
-      // if there are north farmers waiting to cross
-      if (num_north > 0) {
-        // send north farmer across bridge
-        return_val = pthread_create(&north_threads[cur_north_index], NULL, north, NULL);
-
-        // decrement the index to represent that farmers have crossed the bridge
-        cur_north_index--;
-      } else if (num_south > 0) {
-        // no norht farmers waiting, but south farmers waiting
-
-        // send south farmer across bridge
-        return_val = pthread_create(&south_threads[cur_south_index], NULL, south, NULL);
-
-        // decrement the index to represent that farmers have crossed the bridge
-        cur_south_index--;
-      }
+      // decrement the index to represent that farmers have crossed the bridge
+      cur_north_index--;
     }
 
     // check that threads were created successfully
@@ -147,9 +171,15 @@ int main(int argc, char *argv[]) {
       exit(-1);
     }
   }
+  cout << "North and Southbound farmers created, unlocking bridge" << endl;
+  pthread_mutex_unlock(&bridge_mutex);
+
+  // waits until all threads are done
+  while(running_threads != 0) {
+    sleep(1);
+  }
 
   // program finished, all farmers crossed
   cout << "All farmers crossed bridge!" << endl;
-
-
+  return 1;
 }
